7ac37c3aed188a2c39f754a4023b04bc40c697ae
Lavish Dixit <lavishdixit.07@gmail.com>
2025-06-11 09:44:16 +0530

    Delete Add2Numbers.java

--- Add2Numbers.java
+++
@@ -1,8 +0,0 @@
-public class Add2Numbers{
-public static void main(String[] args{
-Scanner sc = new Scanner(System.in);
-
-int a =  sc.nextInt();
-int b =  sc.nextInt(;
-int c =  a + b;
- System.out.println(c);
